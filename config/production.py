a3992c2d457a5f96ce7fb4044191c4c5d5f622f0
roomnine79 <roomnine79@gmail.com>
2025-02-25 09:06:02 +0900

    Configuration Logging

--- config/production.py
+++ config/production.py
@@ -1,7 +1,30 @@
 
 from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xf9\x0b\x9a\xe1\xed\xcb\x00t\xae}\x8bW\x14\xd9\xdc\x0f'
 
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5, # 5MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -11,6 +11,7 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
+    3/0
     return redirect(url_for('question._list'))
 
 
