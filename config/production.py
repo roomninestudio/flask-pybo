8678c8f60e77b92042c2ae4b8d8c9a39202a663a
roomnine79 <roomnine79@gmail.com>
2025-02-25 14:47:42 +0900

    Change Database to PostgreSQL

--- config/production.py
+++ config/production.py
@@ -1,8 +1,15 @@
 
 from config.default import *
 from logging.config import dictConfig
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME')
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xf9\x0b\x9a\xe1\xed\xcb\x00t\xae}\x8bW\x14\xd9\xdc\x0f'
 
